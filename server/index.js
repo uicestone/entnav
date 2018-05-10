0ce2fbd336f2cd88db2228185d6ce687a0255dc9
Uice Lu <uicestone@gmail.com>
2018-05-10 14:55:53 +0800

    使用ejs动态化内容

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const compression = require('compression');
-const app         = express();
-const router      = express.Router();
-const httpServer  = require('http').createServer(app);
-const env         = require('node-env-file');
+const express      = require('express');
+const bodyParser   = require('body-parser');
+const compression  = require('compression');
+const app          = express();
+const router       = express.Router();
+const httpServer   = require('http').createServer(app);
+const env          = require('node-env-file');
+const shimoService = require('./services/shimo');
 
 env(`${__dirname}/../.env`);
 
@@ -15,9 +16,11 @@ app.use(express.query());
 app.use(compression());
 
 require(`${__dirname}/apis`)(app, router);
-app.get('/', (req, res) => {
-  res.render('index.ejs', { message: 'Test' });
+
+app.get('/', async (req, res) => {
+  res.render('index.ejs', { navData: await shimoService.getNavData() });
 });
+
 app.use('/', express.static(`${__dirname}/../`));
 
 const port = process.env.PORT_HTTP || 8080;
